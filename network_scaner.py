c5b1c7c2cfa3040d839a5e0183ff10b72ee73467
Rei Naftali <rehi12380@gmail.com>
2024-09-21 17:29:46 +0300

    Feat(network_scaner):recreate arping scan

--- network_scaner.py
+++ network_scaner.py
@@ -1,8 +1,19 @@
 #!/user/bin/env python
 
 import scapy.all as scapy
+import pprint
+
+from sympy import false
+
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_requset = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_requset_broadcast = broadcast/arp_requset
+    answered_list = scapy.srp(arp_requset_broadcast, timeout=1, verbose=false)[0]
+    print("IP\t\t\tMAC ADDRESS\n------------------------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc +"\t\t"+element[1].hwsrc)
 
+        print("------------------------------------------------------")
 scan("10.0.2.2/24")
